42827b96d7a7a078efa218623c86aaa55f250a3f
FIFCOM <fifcomx@gmail.com>
2020-10-28 15:01:45 +0800

    v1.2

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
 	header("Content-type:text/html;charset=utf-8;");
-    $sdbName = 'example'; //Please modify the Example_String to a random string of length 16
-    $accessToken = 'Example_String'; //Please modify the Example_String to a random string of length 64
+    require_once("config.php");
 	$pastebin=isset($_POST['pastebin'])?base64_encode($_POST['pastebin']):0;
 	if (!$pastebin) {
             $msg = 'Please enter text below.';
         } else {
             if (strlen($pastebin) < 10240||strlen($pastebin) > 0){
-                $pbName = md5($pastebin);
                 $SvrName = $_SERVER['HTTP_HOST'].str_replace('/index.php','',$_SERVER['PHP_SELF']);
-                $mode = 'write';
-                $request = file_get_contents("http://$SvrName/sdb/sdb_$sdbName.php?accessToken=$accessToken&mode=$mode&FileName=$pbName&PasteBin=$pastebin");
+                $pbName = RandCRC32b();
+                PBWrite($pastebin, $pbName);
                 $msg = 'Generated ! ---> <span><a href="https://'.$SvrName.'/'.$pbName.'" target="_blank">https://'.$SvrName.'/'.$pbName.'</a></span>';
             } else {
             $msg = 'PasteBin string is too long. (Requires less than 10KB)';
             }
         }
     require_once("html/index.html.php");
+
+function RandCRC32b()
+{
+    $time = uniqid();
+    return dechex(crc32("P-A-S-T-$time-E-B-I-N"));
+}
+
+function PBWrite($pastebin, $pbName) {
+    $pbName = hash("sha256", $pbName);
+    $file = "pastebin-data/$pbName.pb";
+    $fp = fopen($file, 'w');
+    $content = $pastebin;
+    fwrite($fp, $content . "\n");
+    fclose($fp);
+}
 ?>
 
 <!--
