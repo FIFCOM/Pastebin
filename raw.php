d980a187f0a2663e155e1644713acd9ef1656b18
FIFCOM <1280874899@qq.com>
2021-05-08 23:17:31 +0800

    v2.0 (Beta 2105)

--- raw.php
+++ raw.php
@@ -4,10 +4,16 @@ require_once("pages/pastebinFunctions.php");
 require_once("pages/fidFunctions.php");
 $pastebinFileName = $_REQUEST['id'] ?? "0";
 $pastebinCryptPassword = $_REQUEST['key'] ?? "0";
-//$pastebinRawAccessToken = $_REQUEST['token'] ?? "0";
+$pastebinRawAccessToken = $_REQUEST['token'] ?? "00000000000000000000000000000000";
 $pastebinType = $_REQUEST['type'] ?? "pastebin";
+$pastebinHeaderReferer = $_SERVER["HTTP_REFERER"] ?? 0;
 
-if ($pastebinFileName && $pastebinCryptPassword) {
+if ($pastebinHeaderReferer && pastebinValidateRawAccessToken($pastebinRawAccessToken) == 0) {
+    header('HTTP/1.0 403 Forbidden');
+    exit();
+}
+
+if ($pastebinFileName && $pastebinCryptPassword ) {
     $pastebinRaw = pastebinView($pastebinFileName, $pastebinCryptPassword, $pastebinType);
     header("Content-type:text/plain;charset=utf-8;");
     echo $pastebinRaw;
