3262c889bc9827991364c882a169ead959e8e843
FIFCOM <1280874899@qq.com>
2021-05-30 22:38:25 +0800

    v2.0 (Beta 2105)

--- api.php
+++ api.php
@@ -6,22 +6,22 @@ require_once("pages/fidFunctions.php");
 $mode = $_REQUEST['mode'] ?? 0;
 $pastebinLink = $_REQUEST['pb'] ?? 0;
 
-if ($mode == "raw-json") pastebinRawJson($pastebinLink);
+if ($mode == "raw" && $pastebinLink) pastebinRawJson($pastebinLink);
 
 function pastebinRawJson($pb) {
     $id = pastebinGetSubString(base64_decode($pb), "$", "+");
     $key = dechex(crc32(pastebinGetSubString(base64_decode($pb), "+", "-")));
     if (pastebinView($id, $key, 'title')) {
-        $raw['code'] = "1";
-        $raw['id'] = "$id";
-        $raw['title'] = pastebinView($id, $key, 'title') ? : "null";
-        $raw['content'] = pastebinView($id, $key, 'pastebin') ? : "null";
+        $json['code'] = "1";
+        $json['id'] = "$id";
+        $json['title'] = pastebinView($id, $key, 'title') ? : "null";
+        $json['content'] = pastebinView($id, $key, 'pastebin') ? : "null";
     } else {
-        $raw['code'] = "0";
-        $raw['id'] = "$id";
-        $raw['title'] = "null";
-        $raw['content'] = "null";
+        $json['code'] = "0";
+        $json['id'] = "$id";
+        $json['title'] = "null";
+        $json['content'] = "null";
     }
     header("Content-type:text/plain;charset=utf-8;");
-    echo json_encode($raw, JSON_UNESCAPED_UNICODE);
+    echo json_encode($json, JSON_UNESCAPED_UNICODE);
 }
