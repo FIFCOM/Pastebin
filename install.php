18897c088f48eb9e568faa494467fc9b40710bb1
FIFCOM <1280874899@qq.com>
2021-06-07 22:16:53 +0800

    v2.0 (Beta 2106)

--- install.php
+++ install.php
@@ -3,6 +3,17 @@
 
 $installerStep = installerCheckDatabaseConnection() == 1 ? 2 : 1;
 
+if ($installerStep == 1)
+{
+    $name = $_REQUEST['db-name'];
+    $user = $_REQUEST['db-user'];
+    $host = $_REQUEST['db-host'];
+    $pwd = $_REQUEST['db-pwd'];
+} else if ($installerStep == 2) {
+    $admin_username = $_REQUEST['admin-username'];
+}
+
+
 
 function installerCheckDatabaseConnection() {
 
